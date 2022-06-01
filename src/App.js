3285d617c3140324dc64a634eb25f9cab0d7ab23
ojimanda <yozimanda11@gmail.com>
2022-06-01 15:24:40 +0700

    react shopping cart

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header';
+import Basket from './components/Basket';
+import Main from './components/Main';
+import data from './data';
+import { useState } from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const {products} = data;
+    const [cartItems, setCartItems] = useState([]);
+    /* add product */
+    const onAdd = (product) =>  {
+        const exist = cartItems.find((x) => x.id === product.id);
+        if(exist) {
+            setCartItems(
+                cartItems.map((x) =>
+                    x.id === product.id ? {...exist, qty: exist.qty + 1} : x)
+            )
+        } else {
+            setCartItems([...cartItems, {...product, qty: 1}]);
+        }
+    };
+    /* remove product */
+    const onRemove = (product) => {
+        const exist = cartItems.find((x) => x.id === product.id);
+        if (exist.qty === 1) {
+            setCartItems(cartItems.filter((x) => x.id !== product.id));
+        } else {
+            setCartItems(
+                cartItems.map((x) =>
+                    x.id === product.id ? {...exist, qty: exist.qty - 1} : x)
+            )
+        }
+    }
+    return ( 
+        <div className = "App">
+            <Header countCartItems={cartItems.length}></Header> 
+            <div className='row'>
+                <Main onAdd={onAdd} products={products}></Main> 
+                <Basket onAdd={onAdd} onRemove={onRemove} cartItems={cartItems}></Basket> 
+            </div> 
+        </div>
+    );
 }
 
 export default App;
---
+++ src/data.js
@@ -0,0 +1,24 @@
+const data = {
+    products : [
+        {
+            id: '1',
+            name: 'Hoodie',
+            price: 20,
+            image: '../img/hoodie.jpg',
+        },
+        {
+            id: '2',
+            name: 'T-Shirt',
+            price: 10,
+            image: '../img/tshirt.jpg',
+        },
+        {
+            id: '3',
+            name: 'Jeans',
+            price: 40,
+            image: '../img/jeans.jpg',
+        },
+    ], 
+};
+
+export default data;
